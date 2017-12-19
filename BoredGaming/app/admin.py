c377bc72c485dfa31bcbc30d065254c2b77fceb1
Alison <alison.neer@gmail.com>
2017-12-18 21:02:41 -0500

    HOMEPAGE VIEW FINALLY PULLING FROM DATABASE

--- BoredGaming/app/admin.py
+++ BoredGaming/app/admin.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
 
-from .models import Lead, Profile
+from .models import Lead, Profile, Game
 
 admin.site.register(Lead)
 admin.site.register(Profile)
+admin.site.register(Game)
+
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('email')
--- BoredGaming/app/models.py
+++ BoredGaming/app/models.py
@@ -3,6 +3,7 @@ Definition of models.
 """
 
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -12,11 +13,25 @@ from django.dispatch import receiver
 class Lead(models.Model):
     email_address = models.EmailField()
 
+    def __str__(self):
+        return self.email_address
+
+class Game(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     hometown = models.CharField(max_length=100, blank=True)
     rpg_class = models.CharField(max_length=100, blank=True)
+    games_liked = models.ManyToManyField(Game, related_name='liked')
+    games_owned = models.ManyToManyField(Game, related_name='owned')
+
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -26,6 +41,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-
-
